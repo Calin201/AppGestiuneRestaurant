4c43f908551056edf084c2393610a70b46a062a6
Calin201 <88057934+Calin201@users.noreply.github.com>
2022-03-28 15:26:37 +0300

    Initial commit

---
+++ src/AppGestiuneRestaurant/Models/AlimentBaza.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestiuneRestaurant.Models
+{
+    class AlimentBaza
+    {
+        public string Denumire { get; set; }
+        public int Cantitate { get; set; }
+        public DateTime DataAchizitie { get; set; }
+        public DateTime DataExpirare { get; set; }
+
+        public AlimentBaza()
+        {
+            Denumire = string.Empty;
+            Cantitate = 0;
+            DataAchizitie = DateTime.MaxValue;
+            DataExpirare = DateTime.MinValue;
+        }
+        public AlimentBaza(int _cantitate, string _denumire,DateTime _dataAchizitie,DateTime _dataExpirare)
+        {
+            Denumire = _denumire;
+            Cantitate = _cantitate;
+            DataAchizitie = _dataAchizitie;
+            DataExpirare = _dataExpirare;
+        }
+        
+    }
+}
---
+++ src/AppGestiuneRestaurant/Models/FelMancare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestiuneRestaurant.Models
+{
+    class FelMancare
+    {
+        string Denumire { get; set; }
+        IList<AlimentBaza> ListaIngrediente { get; set; }
+
+    }
+}
