638ecfd553f7f634fa51d6bceca84c3f09540987
Calin201 <88057934+Calin201@users.noreply.github.com>
2022-03-28 15:31:24 +0300

    WIP

--- src/AppGestiuneRestaurant/Program.cs
+++ src/AppGestiuneRestaurant/Program.cs
@@ -10,6 +10,18 @@ namespace AppGestiuneRestaurant
     {
         static void Main(string[] args)
         {
+            var da = new AlimentBaza();
+            Console.WriteLine(da.Cantitate);
+            Console.WriteLine(da.Denumire);
+            Console.WriteLine(da.DataAchizitie);
+            Console.WriteLine(da.DataExpirare);
+            Console.ReadKey();
+            da = new AlimentBaza(69, "Cu Bianca", DateTime.Now, DateTime.Now);
+            Console.WriteLine(da.Cantitate);
+            Console.WriteLine(da.Denumire);
+            Console.WriteLine(da.DataAchizitie);
+            Console.WriteLine(da.DataExpirare);
+            Console.ReadKey();
         }
     }
 }
