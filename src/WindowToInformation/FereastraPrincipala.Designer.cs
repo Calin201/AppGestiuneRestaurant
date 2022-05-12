50723594dd083123c34c096d3c42a07ecb2b2acc
Calin201 <88057934+Calin201@users.noreply.github.com>
2022-05-13 02:16:22 +0300

    WIP

--- src/WindowToInformation/FereastraPrincipala.Designer.cs
+++ src/WindowToInformation/FereastraPrincipala.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace WindowToInformation
 {
     partial class FereastraPrincipala
@@ -29,21 +31,239 @@ namespace WindowToInformation
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle29 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle30 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle31 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle32 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Denumire = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantitate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data_Achizitie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Data_Expirare = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(985, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrediente:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Denumire,
+            this.Cantitate,
+            this.Data_Achizitie,
+            this.Data_Expirare});
+            this.dataGridView1.Location = new System.Drawing.Point(169, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(792, 504);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Denumire
+            // 
+            dataGridViewCellStyle29.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Denumire.DefaultCellStyle = dataGridViewCellStyle29;
+            this.Denumire.HeaderText = "Denumire";
+            this.Denumire.Name = "Denumire";
+            this.Denumire.Width = 77;
+            // 
+            // Cantitate
+            // 
+            dataGridViewCellStyle30.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Cantitate.DefaultCellStyle = dataGridViewCellStyle30;
+            this.Cantitate.HeaderText = "Cantitate";
+            this.Cantitate.Name = "Cantitate";
+            this.Cantitate.Width = 74;
+            // 
+            // Data_Achizitie
+            // 
+            dataGridViewCellStyle31.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Data_Achizitie.DefaultCellStyle = dataGridViewCellStyle31;
+            this.Data_Achizitie.HeaderText = "Data Achizitie";
+            this.Data_Achizitie.Name = "Data_Achizitie";
+            this.Data_Achizitie.Width = 97;
+            // 
+            // Data_Expirare
+            // 
+            dataGridViewCellStyle32.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Data_Expirare.DefaultCellStyle = dataGridViewCellStyle32;
+            this.Data_Expirare.HeaderText = "Data Expirare";
+            this.Data_Expirare.Name = "Data_Expirare";
+            this.Data_Expirare.Width = 96;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Afisare Ingrediente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Afisare Feluri Mancare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 52);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Inregistrare Ingredient";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 230);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(171, 52);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Inregistrare Fel Mancare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(682, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(0, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(220, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(415, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(275, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Inregistreaza Ingredientul";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(879, 104);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(0, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(879, 143);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(0, 20);
+            this.dateTimePicker2.TabIndex = 12;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // FereastraPrincipala
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FereastraPrincipala";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicatie Gestiune Restaurant";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
+       
+
+
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Denumire;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantitate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data_Achizitie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Data_Expirare;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- src/WindowToInformation/FereastraPrincipala.cs
+++ src/WindowToInformation/FereastraPrincipala.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +14,25 @@ using NivelStocareDate;
 namespace WindowToInformation
 {
 
-       
+
     public partial class FereastraPrincipala : Form
     {
-        static AdministrareIngredient_FisierText _administrareIngredient = new AdministrareIngredient_FisierText("Ingrediente.txt");
-        static AdministrareFelMancare_FisierText _administrareFelMancare = new AdministrareFelMancare_FisierText("FeluriMancare.txt");
+        static string numeFisierIngrediente = "Ingrediente.txt";
+        static string numeFisierFeluriMancare = "FeluriMancare.txt";
+        static string locatieFisierSolutie = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        static string caleCompletaFisierIngrediente = locatieFisierSolutie + "\\" + numeFisierIngrediente;
+        static string caleCompletaFisierFeluriMancare = locatieFisierSolutie + "\\" + numeFisierFeluriMancare;
+        AdministrareIngredient_FisierText _administrareIngredient = new AdministrareIngredient_FisierText(caleCompletaFisierIngrediente);
+        AdministrareFelMancare_FisierText _administrareFelMancare = new AdministrareFelMancare_FisierText(caleCompletaFisierFeluriMancare);
 
-        int nrFeluriMancare = 0;
-        int nrIngrediente = 0;
-        FelMancare[] FeluriMancare = _administrareFelMancare.GetFeluriMancare(out int nrFeluriMancare);
-        Ingredient[] Ingrediente = _administrareIngredient.GetIngrediente(out int nrIngrediente);
+        //FelMancare[] FeluriMancare = _administrareFelMancare.GetFeluriMancare(out int nrFeluriMancare);
+        //Ingredient[] Ingrediente = _administrareIngredient.GetIngrediente(out int nrIngrediente);
+
+
+        private const int LATIME_CONTROL = 100;
+        private const int DIMENSIUNE_PAS_Y = 30;
+        private const int DIMENSIUNE_PAS_X = 100;
+        private const int OFFSET_X = 100;
 
 
 
@@ -29,9 +40,140 @@ namespace WindowToInformation
         public FereastraPrincipala()
         {
             InitializeComponent();
+            this.Size = new Size(500, 500);
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(100, 100);
+            this.Font = new Font("Arial", 9, FontStyle.Bold);
+            this.ForeColor = Color.LimeGreen;
+            this.Text = "Gestiune Restaurant";
+            label1.Text = "Ingrediente";
+            dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            Afiseaza_Ingrediente();
+            //Afiseaza_FeluriMancare();
+        }
+        private void Afiseaza_Ingrediente()
+        {
+            Ingredient[] Ingrediente = _administrareIngredient.GetIngrediente(out int nrIngrediente);
+            dataGridView1.ColumnCount = 4;
+            DeleteGridInfo();
+            dataGridView1.Columns[0].Name = "Denumire";
+            dataGridView1.Columns[1].Name = "Cantitate";
+            dataGridView1.Columns[2].Name = "Data Achizitiei";
+            dataGridView1.Columns[3].Name = "Data Expirarii";
+
+            for (int i = 0; i < nrIngrediente; i++)
+            {
+
+                string[] linie = {Ingrediente[i].Denumire, Ingrediente[i].Cantitate.ToString(),
+                    Ingrediente[i].DataAchizitie.ToString("dd/MM/yyyy"),Ingrediente[i].DataExpirare.ToString("dd/MM/yyyy") };
+                dataGridView1.Rows.Add(linie);
+
+            }
+
+        }
+        private void Afiseaza_FeluriMancare()
+        {
+            FelMancare[] FeluriMancare = _administrareFelMancare.GetFeluriMancare(out int nrFeluriMancare);
+
+            dataGridView1.ColumnCount = 2;
+            DeleteGridInfo();
+            dataGridView1.Columns[0].Name = "Denumire";
+            dataGridView1.Columns[1].Name = "Ingrediente";
+
+
+            for (int i = 0; i < nrFeluriMancare; i++)
+            {
+
+                string[] linie = { FeluriMancare[i].Denumire, FeluriMancare[i].ListaIngrediente[0].Denumire };
+                for (int j = 1; j < FeluriMancare[i].ListaIngrediente.Count; j++)
+                {
+                    linie[1] += "," + FeluriMancare[i].ListaIngrediente[0].Denumire;
+                }
+                dataGridView1.Rows.Add(linie);
+            }
+
+        }
+
+
+        private void DeleteGridInfo()
+        {
+            while (dataGridView1.Rows.Count > 0)
+                dataGridView1.Rows.RemoveAt(0);
+        }
+        private void DeleteBoxes()
+        {
+            this.Controls.Remove(textBox1);
+            this.Controls.Remove(textBox2);
+            this.Controls.Remove(dateTimePicker1);
+            this.Controls.Remove(dateTimePicker2);
+            this.Controls.Remove(button5);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DeleteBoxes();
+            this.Controls.Add(dataGridView1);
+            Afiseaza_Ingrediente();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DeleteBoxes();
+            this.Controls.Add(dataGridView1);
+            Afiseaza_FeluriMancare();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(dataGridView1);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(textBox2);
+            this.Controls.Add(dateTimePicker1);
+            this.Controls.Add(dateTimePicker2);
+            this.Controls.Add(button5);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(dataGridView1);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == String.Empty && textBox2.Text != String.Empty )
+            {
+                MessageBox.Show("Denumirea nu a fost adaugata");
+            }
+            else if (textBox1.Text != String.Empty && textBox2.Text == String.Empty )
+            {
+                MessageBox.Show("Cantitatea nu a fost adaugata");
+            }
+            else
+            {
+                
+                var a = dateTimePicker1.Text;
+                var b = dateTimePicker2.Text;
+                int.TryParse(textBox2.Text, out int x);
+                //_administrareIngredient.AddIngredient(new Ingredient(textBox1.Text,x, a,b));
+            }
+        }
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
         {
 
         }
